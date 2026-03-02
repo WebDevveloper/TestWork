dd6cec8d97e73ac8b4b00007193895f191d80d0a
WebDevveloper <truall562gensf@gmail.com>
2026-03-02 15:51:31 +0300

    Добавил gitignore и убрал node_modules

--- dist/core/types.js
+++
@@ -1,6 +0,0 @@
-/**
- * Общие типы для библиотеки
- * ShapeData используется для сериализации, ChangeDetail для события "change"
- */
-export {};
-//# sourceMappingURL=types.js.map
--- dist/core/validator.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Проверяет, что value - конечное число > 0
- * Используется для валидации параметров фигур
- */
-export declare function positiveNumber(value: number, name: string): number;
-//# sourceMappingURL=validator.d.ts.map
